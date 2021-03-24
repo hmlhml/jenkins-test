20f6e3da8db882749c57ec436d50b4228c0f653b
hmlhml <405185142@qq.com>
2021-03-24 10:41:34 +0800

    Add files via upload

---
+++ dist/precache-manifest.f01ba62d8c1812376cd9f73b1c5106c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6f50005f25f83884e4",
+    "url": "/css/app.f5d2450b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "31599f9ef75b25a186e3b554981882e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b36a49e5cf7aeb59c700",
+    "url": "/js/about.3d6b3a0e.js"
+  },
+  {
+    "revision": "3c6f50005f25f83884e4",
+    "url": "/js/app.956fd853.js"
+  },
+  {
+    "revision": "9fd3846724887311ec31",
+    "url": "/js/chunk-vendors.f1eab937.js"
+  },
+  {
+    "revision": "4ab27fa85379d43a348ea4b1c5b6042a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
